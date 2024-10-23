43fbcb2fc534d10c74b94483796560ee41198a01
Vaishnavi-Kharat <vaishnavikharat2020@gmail.com>
2024-10-23 16:39:22 +0530

    01-variableAssign.js
    01-variableAssign.js

---
+++ 01-variableAssign.js
@@ -0,0 +1,36 @@
+var myFullName;
+console.log("My name is :",myFullName);
+
+myFullName = "Vaishnavi Kharat"
+console.log("My name is :",myFullName);
+
+myFullName = "Vaishnavi Bhima Kharat"
+console.log("My full name is :",myFullName);
+
+var myLove = "I love only JS"
+console.log(myLove);
+
+var firstName = "Vaishnavi"
+console.log("My firts name is :",firstName);
+
+var lastName = "Kharat"
+console.log("My last name is :",lastName);
+
+var age = 21
+console.log("My age is :",age);
+
+var address = "City - Pandharpur, Dist- Solapur"
+console.log("My address is :",address);
+
+var pinCode = 413310
+console.log("My pinCode is :",pinCode);
+
+var gender= "Female"
+console.log("My gender is :",gender);
+
+var isMarried= "No"
+console.log("Is I am married  :",isMarried);
+
+
+
+
