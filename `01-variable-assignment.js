61981cc882f7fd0a06a158960bec629f9ffe6fc9
Vaishnavi-Kharat <vaishnavikharat2020@gmail.com>
2024-10-23 16:19:58 +0530

    01-variable-assignment.js
    01-variable-assignment.js

---
+++ `01-variable-assignment.js
@@ -0,0 +1,19 @@
+var city;     // declaration
+city="Sangli";   //initialization
+console.log("My city name is : ",city);
+
+var friendName = "Navamika"     // declaration and initialization
+console.log("My bestfriend name is :",friendName);
+
+var countryName = "India"
+console.log("My country name is :",countryName);
+
+var schoolName = "New English School Bhalawani"
+console.log("My school name is :",schoolName);
+
+var collegeName = "RIT,Islampur"
+console.log("My college name is :",collegeName);
+
+
+
+
