01a1285b1e516573936d86e9df454e457ba95fa7
Vaishnavi-Kharat <vaishnavikharat2020@gmail.com>
2024-10-24 22:08:35 +0530

    03-funAsign01.js
    03-funAsign01.js

---
+++ 03-funAsign01.js
@@ -0,0 +1,27 @@
+function message(){
+    console.log("Hello,Good Morning");
+}
+message();
+console.log("============================");
+
+function country(){
+    console.log("My country name is India");
+}
+country();
+console.log("============================");
+
+function personalDetails(firstName,lastName,collegeName){
+    console.log("My name is",firstName,lastName);
+    console.log("My college name is",collegeName);
+}
+personalDetails("Vaishnavi","Kharat","RIT,Isalmpur");
+console.log("============================");
+
+function addThreeValues(n1,n2,n3){
+    console.log("The values are :",n1,n2,n3);
+    var result =n1+n2+n3;
+    console.log("Addition of three number is :-",result);
+}
+addThreeValues(10.23, 600, 40);
+addThreeValues("Hello"," Good"," Morning")
+
