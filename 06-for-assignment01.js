403c8c2c5fd174969500b3107186c0f2ebc7b728
Vaishnavi-Kharat <vaishnavikharat2020@gmail.com>
2024-12-11 17:13:12 +0530

    06-for-assignment01.js
    06-for-assignment01.js

---
+++ 06-for-assignment01.js
@@ -0,0 +1,40 @@
+console.log('---------------- Print number from 5 to 15 ---------------');
+for (let index = 5; index <= 15; index++) {
+    console.log(index);
+}
+
+console.log('---------------- Print number from 50 to 40 in reverse ---------------');
+for (let index = 50; index >= 40; index--) {
+    console.log(index);
+}
+
+console.log('---------------- WAP to find first 15 ODD numbers ---------------');
+for (let index = 1; index <=30; index=index+2) {
+    console.log(index);
+}
+    
+console.log('---------------- WAP to find first 10 even numbers ---------------');
+for (let index = 2; index <=20; index=index+2) {
+    console.log(index);
+}
+
+console.log('-----------------Table of 5----------------');
+for (let index = 5; index <=50; index=index+5) {
+    console.log(index);
+}
+
+// Another way to print a 5 table
+// console.log('=========== Table of 5 ============');
+// for (let index = 1; index <=10; index++) {
+//     console.log(index*5);
+// }
+
+console.log('----------------Table of 10---------------');
+for (let index = 10; index <=100; index=index+10) {
+    console.log(index);
+}
+
+console.log('----------------Table of 10 in reverse order---------------');
+for (let index = 100; index >=10; index=index-10) {
+    console.log(index);
+}
